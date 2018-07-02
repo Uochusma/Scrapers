e8259335c4b17dd67c4246496801773cf2745346
suzukishota22 <27115076@stn.nitech.ac.jp>
2018-07-02 16:10:58 +0900

    ファイルへのセーブ機能を追加した．

--- gooScraper.py
+++ gooScraper.py
@@ -1,4 +1,4 @@
-## -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -11,43 +11,50 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-#===================================================================================================
+# ===================================================================================================
 starttime = time.ctime()
 phase = 1
-#===================================================================================================
-print("phase ",phase," start", "=" * 60, time.ctime())
+# ===================================================================================================
+print("phase ", phase, " start", "=" * 60, time.ctime())
+
+
 def getQustionURL(aTopURL):
     html = urllib.request.urlopen(url)
     soup = BeautifulSoup(html)
-    #print(soup.body)
+    # print(soup.body)
     result_list = soup.find('div', attrs={'class': 'r_list'})
-    #print(result_list)
-    result_list_content = result_list.ul.find_all('li', attrs={'class': 'clearfix'})
+    # print(result_list)
+    result_list_content = result_list.ul.find_all(
+        'li', attrs={'class': 'clearfix'})
     #aref_list = soup.body.findAll('div')
     #total = len(soup.body.findAll('a'))
     q_url_list = []
-    for i,result in enumerate(result_list_content):
+    for i, result in enumerate(result_list_content):
         print(i)
-        #print(result)
+        # print(result)
         text = result.find('div', attrs={'class': 'result'})
         print(text)
         q_url = text.ul.li.h2.a.attrs['href']
         q_url_list.append(q_url)
     return(q_url_list)
 
+
 # アクセスするURL
 url = "https://oshiete.goo.ne.jp/articles/qa/"
 question_url = getQustionURL(url)
 print(question_url)
-print("phase ",phase," done", "=" * 60, time.ctime())
+print("phase ", phase, " done", "=" * 60, time.ctime())
 
-phase+=1
-#===================================================================================================
-print("phase ",phase," start", "=" * 60, time.ctime())
+phase += 1
+# ===================================================================================================
+print("phase ", phase, " start", "=" * 60, time.ctime())
+
 
 def getContent(aQuestionURL):
     q_html = urllib.request.urlopen('https:'+aQuestionURL)
     q_soup = BeautifulSoup(q_html)
+    qID = aQuestionURL.replace('https://oshiete.goo.ne.jp/qa/', '')
+    qID = qID.replace('.html', '')
     print("q_soup.body")
 #     print(q_soup.body)
 #     questionDiv = q_soup.find('div',attrs={'class': 'ptsQes'})
@@ -56,10 +63,10 @@ def getContent(aQuestionURL):
     question_list = q_soup.body.find('p', attrs={'class': 'q_text'})
 #     print(question_list)
     question_text = ''
-    for i,q in enumerate(question_list):
-#         print('='*60)
-#         print(i)
-#         print(q)
+    for i, q in enumerate(question_list):
+        #         print('='*60)
+        #         print(i)
+        #         print(q)
         q_text = str(q)
 #         print(q_text)
 #         if q_text == '<br/>':
@@ -75,15 +82,16 @@ def getContent(aQuestionURL):
 #         print(question_text)
     print('*****Question*****')
     print(question_text)
-    #スリープ
+    # スリープ
     time.sleep(1)
 
     answer_list = q_soup.body.find_all('div', attrs={'class': 'a_text'})
 #     print(answer_list)
+    answer_text_list = []
     for i, a in enumerate(answer_list):
-#         print('='*60)
-#         print(i)
-#         print(a)
+        #         print('='*60)
+        #         print(i)
+        #         print(a)
         a_text = str(a)
 #         for j, a_line in enumerate(a_text)
         a_text = re.sub(r'<div.*>', '', a_text)
@@ -94,31 +102,52 @@ def getContent(aQuestionURL):
         a_text = re.sub(r'\s', '', a_text)
         print('*****Answer*****')
         print(a_text)
+        answer_text_list.append(a_text)
+    time.sleep(1)
+    if(len(question_text) > 0 and len(answer_text_list) > 0):
+        return(question_text, answer_text_list, qID)
+    else:
+        return(None, None, None)
 
+
+#save_dir = os.getcwd()
+save_dir = '/home/itolab/virtualenvs/sharedData/Q&A/goo'
+
+
+def saveContentS(aQuestion_URL_list):
+    for i, qURL in enumerate(aQuestion_URL_list):
+        print(qURL)
+        qText, aTextList, qID = getContent(qURL)
+        if(qText != None and aTextList != None):
+            # print(save_dir+'/Question/'+'Q'+qID+'.txt')
+            pathQ = save_dir+"/Question/"+"Q"+qID+".txt"
+            fQ = open(pathQ, 'w')
+            fQ.write(qText)
+            fQ.close()
+            pathA = save_dir+"/Answer/"+"A"+qID+".txt"
+            fA = open(pathA, 'w')
+            fA.writelines(aTextList)
+            fA.close()
     return
 
-save_dir = os.getcwd()
-for i,qURL in enumerate(question_url):
-    print(qURL)
-    getContent(qURL)
-    """
-    if(ref.attrs['href'].find('.log')==-1):
-        continue
-    #print(ref)
-    #print("%s's url is %s" % (ref.text, ref.attrs['href']))
-    file_name = ref.attrs['href']
-    print(i,'/',total,':',file_name)
-    download_url = url+file_name
-    #print(download_url)
-    r = requests.get(download_url)
-    #print('content=',r.content)
-    #print('content2=',r.content.decode('utf-8'))
-    text = r.content.decode('utf-8')
-    # ファイルの保存
-    if r.status_code == 200:
-        #f = open(save_dir+'/'+file_name+'.txt', 'w','utf-8')
-        f = codecs.open(save_dir+'/train2/'+file_name+'.txt', 'w', 'utf-8')
-        f.write(text)
-        f.close()
-    #"""
 
+saveContentS(question_url)
+#
+import datetime
+
+
+def scraping():
+    #starttime = time.ctime()
+    starttime = datetime.datetime.now()  # 現在の日時を取得
+    while((datetime.datetime.now()-starttime).seconds < 600):
+        # アクセスするURL
+        # url = "https://okwave.jp/"
+        url = "https://oshiete.goo.ne.jp/articles/qa/"
+        question_url = getQustionURL(url)
+        save_dir = '/home/itolab/virtualenvs/sharedData/Q&A/goo'
+        saveContentS(question_url)
+        time.sleep(60)
+    return
+
+
+scraping()
--- yahooScraper.py
+++ yahooScraper.py
@@ -1,4 +1,4 @@
-## -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -10,52 +10,58 @@ import urllib.request
 import requests
 import time
 from bs4 import BeautifulSoup
-#===================================================================================================
+# ===================================================================================================
 starttime = time.ctime()
 phase = 1
-#===================================================================================================
-print("phase ",phase," start", "=" * 60, time.ctime())
+# ===================================================================================================
+print("phase ", phase, " start", "=" * 60, time.ctime())
+
+
 def getQustionURL(aTopURL):
     html = urllib.request.urlopen(url)
     soup = BeautifulSoup(html)
-    #print(soup.body)
+    # print(soup.body)
     q_url_list = []
     all_q_list = soup.find_all('div', attrs={'class': 'questionList'})
     for q_list in all_q_list:
         q_list_content = q_list.find_all('li')
-        for i,ref in enumerate(q_list_content):
-            #print(ref)
+        for i, ref in enumerate(q_list_content):
+            # print(ref)
             q_url = ref.find('a').attrs['href']
             q_url_list.append(q_url)
     return(q_url_list)
 
+
 # アクセスするURL
 url = "https://chiebukuro.yahoo.co.jp/"
 question_url = getQustionURL(url)
-#print(question_url)
-print("phase ",phase," done", "=" * 60, time.ctime())
-phase+=1
-#===================================================================================================
-print("phase ",phase," start", "=" * 60, time.ctime())
+# print(question_url)
+print("phase ", phase, " done", "=" * 60, time.ctime())
+phase += 1
+# ===================================================================================================
+print("phase ", phase, " start", "=" * 60, time.ctime())
+
+
 def getContent(aQuestionURL):
     print('='*50)
     print("*Question*")
     print(aQuestionURL)
     q_html = urllib.request.urlopen(aQuestionURL)
     q_soup = BeautifulSoup(q_html)
-    #print(q_soup.body)
+    # print(q_soup.body)
     #questionDiv = q_soup.find('div', attrs={'class': 'ptsQes'})
     #questionDiv1 = q_soup.body.find_all('div', attrs={'class': 'mdPstd mdPstdQstn sttsRslvd  clrfx'})
-    questionDiv1 = q_soup.body.find_all('div', attrs={'class': re.compile('mdPstdQstn')})[0]
-    #print(questionDiv1)
+    questionDiv1 = q_soup.body.find_all(
+        'div', attrs={'class': re.compile('mdPstdQstn')})[0]
+    # print(questionDiv1)
     questionDiv2 = questionDiv1.find('div', attrs={'class': 'ptsQes'})
     questionList = questionDiv2.find_all('p')
-    #print(questionList)
+    # print(questionList)
     total_text = ''
-    for i,q in enumerate(questionList):
+    for i, q in enumerate(questionList):
         print(i)
-        #print('*'*30)
-        print('text=',q.text)
+        # print('*'*30)
+        print('text=', q.text)
         """
         if(q.p!=None):
             print('p=',q.p.text)
@@ -64,87 +70,96 @@ def getContent(aQuestionURL):
         if(q.span!=None):
             print('span=',q.span.text)
         #"""
-        #q.find('br').extract()
+        # q.find('br').extract()
         text = q.text
-        #print('text_replaced1=',text)
+        # print('text_replaced1=',text)
         #text = text.replace('\s','')
         text = re.sub(r'\s', "", text)
-        #print('text_replaced2=',text)
+        # print('text_replaced2=',text)
         #text = text.replace(r'\s','')
-        text = text.replace('   ','')
-        #print('text_replaced3=',text)
-        text = text.replace(r'\n','')
+        text = text.replace('   ', '')
+        # print('text_replaced3=',text)
+        text = text.replace(r'\n', '')
         text = re.sub(r'\n', "", text)
-        #print('text_replaced4=',text)
+        # print('text_replaced4=',text)
         text = re.sub(r'\t', "", text)
-        #print('text_replaced5=',text)
+        # print('text_replaced5=',text)
         text = re.sub(r'\v', "", text)
-        #print('text_replaced6=',text)
+        # print('text_replaced6=',text)
         #text = text.replace(r'^違反報告$','')
         text = re.sub(r'^違反報告$', "", text)
-        #print('text_replaced7=',text)
-        print('text_replaced=',text)
-        #print('*'*30)
-        #print('len=',len(text))
-        if(len(text)!=0):
-            total_text+=text
-    qID = aQuestionURL.replace('https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/','')
+        # print('text_replaced7=',text)
+        print('text_replaced=', text)
+        # print('*'*30)
+        # print('len=',len(text))
+        if(len(text) != 0):
+            total_text += text
+    qID = aQuestionURL.replace(
+        'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/', '')
     print(qID)
-    print('total=',total_text)
+    print('total=', total_text)
     print("*Answer*")
     answer_text = ''
     answerDiv1 = q_soup.body.find('div', attrs={'id':  re.compile('ba')})
-    if(answerDiv1!=None):    
+    if(answerDiv1 != None):
         answerDiv2 = answerDiv1.find('div', attrs={'class': 'ptsQes'})
         answerList = answerDiv2.find_all('p')
-        for i,q in enumerate(answerList):
-            #print('text=',q.text)
+        for i, q in enumerate(answerList):
+            # print('text=',q.text)
             text = q.text
             text = re.sub(r'\s', "", text)
-            text = text.replace('   ','')
-            text = text.replace(r'\n','')
+            text = text.replace('   ', '')
+            text = text.replace(r'\n', '')
             text = re.sub(r'\n', "", text)
             text = re.sub(r'\t', "", text)
             text = re.sub(r'\v', "", text)
             text = re.sub(r'^違反報告$', "", text)
-            #print('text_replaced=',text)
-            if(len(text)!=0):
-                answer_text+=text
-    if(len(answer_text)!=0):
-        print('answer=',answer_text)
+            # print('text_replaced=',text)
+            if(len(text) != 0):
+                answer_text += text
+    if(len(answer_text) != 0):
+        print('answer=', answer_text)
     else:
         print("No Answer")
-    #スリープ
+    # スリープ
     time.sleep(1)
-    if(len(total_text)>0 and len(answer_text)>0):
-        return(total_text,answer_text,qID)
+    if(len(total_text) > 0 and len(answer_text) > 0):
+        return(total_text, answer_text, qID)
     else:
-        return(None,None,None)
+        return(None, None, None)
+
+
 #save_dir = os.getcwd()
 save_dir = '/home/itolab/virtualenvs/sharedData/Q&A/yahoo'
 #save_dir = '/home/itolab/notebooks/yoshino/Data/Q&A/yahoo'
+
+
 def saveContentS(aQuestion_URL_list):
-    for i,qURL in enumerate(aQuestion_URL_list):
+    for i, qURL in enumerate(aQuestion_URL_list):
         print(qURL)
-        qText,aTextList,qID = getContent(qURL)
-        if(qText!=None and aTextList!=None):
-            #print(save_dir+'/Question/'+'Q'+qID+'.txt')
+        qText, aTextList, qID = getContent(qURL)
+        if(qText != None and aTextList != None):
+            # print(save_dir+'/Question/'+'Q'+qID+'.txt')
             pathQ = save_dir+"/Question/"+"Q"+qID+".txt"
-            fQ = open(pathQ,'w')
+            fQ = open(pathQ, 'w')
             fQ.write(qText)
             fQ.close()
             pathA = save_dir+"/Answer/"+"A"+qID+".txt"
-            fA = open(pathA,'w')
+            fA = open(pathA, 'w')
             fA.writelines(aTextList)
             fA.close()
     return
+
+
 saveContentS(question_url)
 #
 import datetime
+
+
 def scraping():
     #starttime = time.ctime()
-    starttime = datetime.datetime.now() # 現在の日時を取得
-    while((datetime.datetime.now()-starttime).seconds<600):
+    starttime = datetime.datetime.now()  # 現在の日時を取得
+    while((datetime.datetime.now()-starttime).seconds < 600):
         # アクセスするURL
         url = "https://okwave.jp/"
         question_url = getQustionURL(url)
@@ -152,4 +167,6 @@ def scraping():
         saveContentS(question_url)
         time.sleep(60)
     return
+
+
 scraping()
